ab7f2b122db34ed8c5f91a5c57dbda3f4de1238e
ZitelliDZ <zitelli.dz@gmail.com>
2023-03-31 15:47:26 -0300

    Deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '994ea015d50aa3536710f92da194f8e1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '44a4a950-4322-47b4-9d95-b59ece40d6f1'
+};
